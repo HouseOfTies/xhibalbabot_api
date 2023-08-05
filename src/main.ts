7b26a6a6fbdcceaeef5826624414e9fcb62cfe2b
Seventty <elmastigeron2015@gmail.com>
2023-08-05 15:31:19 -0400

    init commit

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Xhibalbabot API')
+    .setDescription('This api serve the bot data')
+    .setVersion('1.0')
+    .addTag('Bot', 'Telegram bot')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
